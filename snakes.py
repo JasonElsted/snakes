a6fd5ba8cee8f2695b39f5cf4871c436f0f2df3d
JasonElsted <jason_maxi15@yahoo.com>
2020-01-27 17:11:15 -0500

    Optimized code for movement of snake (from O(n) to O(1))

--- snakes.py
+++ snakes.py
@@ -63,35 +63,70 @@ createBorder()
 eaten = False
 level = 1
 
+#create an empty list to be filled with lines that make up the snake
+line = []
+
+generateSnake(line, snake)
+
+#draw the snake
+for i in range(len(line)):
+    line[i].draw(win)
+    line[i].setOutline('green')
+    line[i].setWidth(3)
+
+#generate and draw head of the snake
+head = Circle(Point(snake[0].getX(),snake[0].getY()),1)
+head.draw(win)
+head.setWidth(4)
+head.setOutline('red')
+
+pointHit = True
+
 #check that snake has not eaten itself and it is still within the border
 while (eaten == False and snake[0].getX() > win.getWidth() / 2 - 280 and snake[0].getX() < win.getWidth() / 2 + 280 and snake[0].getY() > win.getHeight() / 2 - 280 and snake[0].getY() < win.getHeight() / 2 + 280):
+    if pointHit:
+        #generate and draw level message
+        levelMessage = Text(Point(win.getWidth() - 60, win.getHeight() - 10), "Level {}".format(level))
+        levelMessage.draw(win)
+        levelMessage.setOutline('white')
+        levelMessage.setFace('courier')
 
-    #create an empty list to be filled with lines that make up the snake
-    line = []
-    
-    #generate and draw level message
-    levelMessage = Text(Point(win.getWidth() - 60, win.getHeight() - 10), "Level {}".format(level))
-    levelMessage.draw(win)
-    levelMessage.setOutline('white')
-    levelMessage.setFace('courier')
+        #draw the randomly generated point
+        point = Circle(pointCenter, 1)
+        point.draw(win)
+        point.setOutline('white')
+        point.setFill('white')
+        point.setWidth(4)
 
     pointHit = False
 
-    #draw the randomly generated point
-    point = Circle(pointCenter, 1)
-    point.draw(win)
-    point.setOutline('white')
-    point.setFill('white')
-    point.setWidth(4)
+    #move the whole snake by 10 pixels in a certain direction
+    firstPoint = Point(snake[0].getX() + dx, snake[0].getY() + dy)
+    snake = [firstPoint] + snake
+    snake = snake[0:len(snake) - 1]
+    firstLine = Line(snake[0], snake[1])
+    line = [firstLine] + line
+    line[len(line) - 1].undraw()
+    head.undraw()
 
-    #generate the snake
-    generateSnake(line, snake)
+    #check if snake hits the point
+    if (snake[0].getX() == pointCenter.getX() and snake[0].getY() == pointCenter.getY()):
+        #delete the point and level message
+        point.undraw()
+        levelMessage.undraw()
 
-    #draw the snake
-    for i in range(len(line)):
-        line[i].draw(win)
-        line[i].setOutline('green')
-        line[i].setWidth(3)
+        pointHit = True
+        level = level + 1   #player levels up
+        pointCenter = generatePoint(snake)   #generate a new random point
+        snake.append(Point(2 * snake[len(snake)-1].getX() - snake[len(snake)-2].getX(), 2 * snake[len(snake)-1].getY() - snake[len(snake)-2].getY()))   #increase length of the snake
+        newLine = Line(snake[len(snake) - 1], snake[len(snake) - 2])
+        line.append(newLine)
+        line[len(line) - 1].draw(win)
+
+    line[0].draw(win)
+    line[0].setOutline('green')
+    line[0].setWidth(3)
+    line = line[0:len(line) - 1]
 
     #generate and draw head of the snake
     head = Circle(Point(snake[0].getX(),snake[0].getY()),1)
@@ -99,18 +134,8 @@ while (eaten == False and snake[0].getX() > win.getWidth() / 2 - 280 and snake[0
     head.setWidth(4)
     head.setOutline('red')
 
-    #move the whole snake by 10 pixels in a certain direction
-    for i in range(len(snake)-1,0,-1):
-        snake[i]= Point(snake[i-1].getX(), snake[i-1].getY())
-    snake[0].move(dx,dy)
-
     #make a delay to show snake before deleting
-    time.sleep(0.15)
-
-    #delete the snake and its head
-    for i in range(len(line)):
-        line[i].undraw()
-    head.undraw()
+    time.sleep(0.1)
 
     #get which key is pressed, if any
     key = win.checkKey()
@@ -136,13 +161,6 @@ while (eaten == False and snake[0].getX() > win.getWidth() / 2 - 280 and snake[0
             dy = 0
             state = 'horizontal'
 
-    #check if snake hits the point
-    if (snake[0].getX() == pointCenter.getX() and snake[0].getY() == pointCenter.getY()):
-        pointHit = True
-        level = level + 1   #player levels up   
-        pointCenter = generatePoint(snake)   #generate a new random point
-        snake.append(Point(2 * snake[len(snake)-1].getX() - snake[len(snake)-2].getX(), 2 * snake[len(snake)-1].getY() - snake[len(snake)-2].getY()))   #increase length of the snake
-
     #check if snake has eaten itself
     i = 1
     while (i < len(snake) and eaten == False):
@@ -150,33 +168,12 @@ while (eaten == False and snake[0].getX() > win.getWidth() / 2 - 280 and snake[0
             eaten = True
         i = i + 1
 
-    #delete the point and level message
-    point.undraw()
-    levelMessage.undraw()
-
-#generate and draw snake and head one more time to show crash
-generateSnake(line, snake)  
-for i in range(len(line)):
-    line[i].draw(win)
-    line[i].setOutline('green')
-    line[i].setWidth(3)
-
-head = Circle(Point(snake[0].getX(),snake[0].getY()),1)
-head.draw(win)
-head.setWidth(4)
-head.setOutline('red')
-
-#make a short delay, then delete snake and head
+#make a short delay, then delete snake and point
 time.sleep(0.5)
 for i in range(len(line)):
     line[i].undraw()
 head.undraw()
-
-#draw level message showing final level
-levelMessage = Text(Point(win.getWidth() - 60, win.getHeight() - 10), "Level {}".format(level))
-levelMessage.draw(win)
-levelMessage.setOutline('white')
-levelMessage.setFace('courier')
+point.undraw()
 
 #show game over message
 GameOver()
